dbc5b144674b232ffc7c356e23875a1b527fbc04
Joo Martin <m4rt1n.j00@gmail.com>
2017-05-14 22:26:45 +0200

    Column Info test

--- test/database/column-info.test.js
+++ test/database/column-info.test.js
@@ -20,4 +20,42 @@ describe('ColumnInfo', () => {
             expect(info.isPrimaryKey()).to.be.false;
         });
     });
+
+    describe('#getOptions', () => {
+        it('should return column options as an object', () => {
+            let info = new ColumnInfo({
+                Null: 'NO',
+                Default: 'Value',
+                Key: 'UNI'
+            });
+
+            let options = info.getOptions();
+
+            expect(options.null).to.be.false;
+            expect(options.default).to.be.equal('Value');
+            expect(options.unique).to.be.true;
+        });
+
+        it('should return column options as an object excluding non existing options', () => {
+            let info = new ColumnInfo({
+                Default: 'Value',
+            });
+
+            let options = info.getOptions();
+
+            expect(options.null).to.be.undefined;
+            expect(options.default).to.be.equal('Value');
+            expect(options.unique).to.be.undefined;
+        });
+
+        it('should return null when column does not have options', () => {
+            let info = new ColumnInfo({
+                Type: 'INT (10)'
+            });
+
+            let options = info.getOptions();
+
+            expect(options).to.be.null;
+        });
+    });
 });
