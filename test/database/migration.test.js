d6f38b395c68c43c59b0d781b17dfd5437b1e2cd
Joo Martin <m4rt1n.j00@gmail.com>
2017-05-15 13:43:46 +0200

    Migration ordering

--- test/database/migration.test.js
+++ test/database/migration.test.js
@@ -1,50 +1,79 @@
 const expect = require('chai').expect;
-const app = require('../../database/migration');
+const _ = require('lodash');
+
+const migration = require('../../database/migration');
 
 describe('#App', () => {
     describe('#getOrderedMigrations()', () => {
         it('should get all migrations in order based on their dependencies', () => {
-            let migrations = {
-                categories: {
-                    table: 'categories',
-                    allDependencyOrdered: false,
-                    dependencies: [
-                        {
-                            sourceTable: 'categories',                            
-                            referencedTable: 'users',
-                            referencedColumn: 'id',
-                            updateRule: 'CASCADE',
-                            deleteRule: 'CASCADE'
-                        }
-                    ]
-                },
-                todos: {
-                    table: 'todos',
-                    allDependencyOrdered: false,
-                    dependencies: [
-                        {
-                            sourceTable: 'todos',
-                            referencedTable: 'categories',
-                            referencedColumn: 'id',
-                            updateRule: 'CASCADE',
-                            deleteRule: 'CASCADE'
-                        }
-                    ]
-                },
-                users: {
-                    table: 'users',
-                    allDependencyOrdered: true,
-                    dependencies: []
-                }
-            };
+            let orderedMigrations = migration.getOrderedMigrations(createMigrationData());
 
-            let orderedMigrations = app.getOrderedMigrations(migrations);
+            verifyOrderedMigrations(orderedMigrations);
+        });
 
-            expect(orderedMigrations.length).to.be.equal(3);
+        // Regression test
+        it('should work independetly from the input order', () => {
+            let data = createMigrationData();
+            let migrations = { todos: data.todos, users: data.users, categories: data.categories };
+            let orderedMigrations = migration.getOrderedMigrations(migrations);
 
-            expect(orderedMigrations[0].table).to.be.equal('users');
-            expect(orderedMigrations[1].table).to.be.equal('categories');
-            expect(orderedMigrations[2].table).to.be.equal('todos');
+            verifyOrderedMigrations(orderedMigrations);
+
+            data = createMigrationData();
+            migrations = { users: data.users, todos: data.todos, categories: data.categories };
+            orderedMigrations = migration.getOrderedMigrations(migrations);
+
+            verifyOrderedMigrations(orderedMigrations);
+
+            data = createMigrationData();
+            migrations = { users: data.users, categories: data.categories, todos: data.todos };
+            orderedMigrations = migration.getOrderedMigrations(migrations);
+
+            verifyOrderedMigrations(orderedMigrations);
         });
     });
 });
+
+function verifyOrderedMigrations(orderedMigrations) {
+    expect(orderedMigrations.length).to.be.equal(3);
+
+    expect(orderedMigrations[0].table).to.be.equal('users');
+    expect(orderedMigrations[1].table).to.be.equal('categories');
+    expect(orderedMigrations[2].table).to.be.equal('todos');
+}
+
+function createMigrationData() {
+    return {
+        categories: {
+            table: 'categories',
+            allDependencyOrdered: false,
+            dependencies: [
+                {
+                    sourceTable: 'categories',
+                    referencedTable: 'users',
+                    referencedColumn: 'id',
+                    updateRule: 'CASCADE',
+                    deleteRule: 'CASCADE'
+                }
+            ]
+        },
+        todos: {
+            table: 'todos',
+            allDependencyOrdered: false,
+            dependencies: [
+                {
+                    sourceTable: 'todos',
+                    referencedTable: 'categories',
+                    referencedColumn: 'id',
+                    updateRule: 'CASCADE',
+                    deleteRule: 'CASCADE'
+                }
+            ]
+        },
+        users: {
+            table: 'users',
+            allDependencyOrdered: true,
+            dependencies: []
+        }
+    };
+}
