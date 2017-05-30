22fdcdd41c51f15416f30ccef27ad4f8d24890f0
Joo Martin <m4rt1n.j00@gmail.com>
2017-05-30 09:52:31 +0200

    App js

--- app.js
+++ app.js
@@ -1,118 +1,62 @@
 const fs = require('fs');
 const ejs = require('ejs');
 const _ = require('lodash');
-const mysql = require('mysql');
 const chalk = require('chalk');
 const util = require('util');
+const cluster = require('cluster');
 
+const connection = require('./database/connection');
 const createColumnInfo = require('./database/column-info/factory');
 const createTypeMapper = require('./database/type-mapper/factory');
 const query = require('./database/query');
 const file = require('./file/file');
+const utils = require('./utils/utils');
 
 const config = require('./config.json');
 const typeMapper = createTypeMapper(config.migrationLib);
 
-console.log(chalk.green('********************************************************'));
-console.log(chalk.green('*                                                      *'));
-console.log(chalk.green('*                 Migration Generator                  *'));
-console.log(chalk.green('*               GreenTech Innovacio Zrt.               *'));
-console.log(chalk.green('*                                                      *'));
-console.log(chalk.green('********************************************************'));
+utils.logHeader(config);
 
-util.log(chalk.yellow(`Generating initial migrations for database ${chalk.bold(config.database)}...`));
-util.log(chalk.yellow(`View tables, procedures, triggers, static contents, dependencies will be created`));
-console.log('--------');
+let fileNames = [];
+let allTables = [];
 
-const connection = mysql.createConnection({
-    host: config.host,
-    port: config.port,
-    user: config.user,
-    password: config.password,
-    database: config.database
-});
+let viewTablesPromise = query.getViewTables(connection, query.escapeQuotes)
+    .then(viewTables => file.getViewTablesTemplate(viewTables, config, ejs))
+    .then(template => file.generateFile(template, `${utils.getDate()}1_create_view_tables.php`, config, fs))
+    .then(utils.sideEffect(filename => console.log(`${filename} was generated successfully`)))
+    .catch(err => console.log(chalk.bgRed(err)));
 
-connection.connect();
+let proceduresPromise = query.getProcedures(connection, query.convertProceduresToObjects, query.escapeQuotes)
+    .then(procedures => file.getProcedureTemplate(procedures, config, ejs))
+    .then(template => file.generateFile(template, `${utils.getDate()}2_create_procedures.php`, config, fs))
+    .then(utils.sideEffect(filename => console.log(`${filename} was generated successfully`)))
+    .catch(err => console.log(chalk.bgRed(err)));
 
-const tableKey = `Tables_in_${config.database}`;
-let i = 0;
+let triggersPromise = query.getTriggers(connection, query.escapeQuotes, _)
+    .then(triggers => file.getTriggersTemplate(triggers, config, ejs))
+    .then(template => file.generateFile(template, `${utils.getDate()}3_create_triggers.php`, config, fs))
+    .then(utils.sideEffect(filename => console.log(`${filename}_create_view_tables.php was generated successfully`)))
+    .catch(err => console.log(chalk.bgRed(err)));
 
-let tablesPromise = query.getTableData(connection, query, config)
-    .then(tables => {
-        let length = Object.keys(tables).length;
-        for (table in tables) {
-            (function (index) {
-                file.getTemplate(tables[table], typeMapper, config, createColumnInfo, ejs)
-                    .then(data => {
-                        let fileName = `${(new Date).getTime()}${index}_create_${data.table}_table.php`;
-                        file.generateFile(data.html, fileName, config, fs)
-                            .then(fileName => {
-                                util.log(`${data.table} was generated successfully`);
-                            })
-                            .catch(err => console.log(chalk.bgRed(err)));
+let tableDataPromise = query.getTableData(connection, query, config)
+    .then(utils.sideEffect(tables => fileNames = file.getFileNames(new Date, tables, file)))
+    .then(utils.sideEffect(tables => allTables = tables))
+    .then(tables => file.getTemplates(tables, typeMapper, config, createColumnInfo, ejs, file))
+    .then(templates => file.generateFiles(templates, fileNames, config, fs, file))
+    .catch(err => console.log(chalk.bgRed(err)));
 
-                    })
-                    .catch(err => console.log(chalk.bgRed(err)));
-            }(i));
-            i++;
-        }
+let foreignKeyTemplate = tableDataPromise
+    .then(res =>
+        file.getForeignKeyTemplate(allTables, config, ejs)
+            .then(template => file.generateFile(template, `${utils.getDate()}4_add_foreign_keys.php`, config, fs))
+            .then(utils.sideEffect(filename => console.log(`${filename} was generated successfully`)))
+            .catch(err => console.log(chalk.bgRed(err)))
+    )
+    .catch(console.log);
 
-        let foreignKeyPromise = file.getForeignKeyTemplate(tables, config, ejs)
-            .then(html => {
-                let fileName = `${(new Date).getTime()}${length}_add_foreign_keys.php`;
-                file.generateFile(html, fileName, config, fs)
-                    .then(fileName => {
-                        util.log(`Foreign keys was generated successfully`);
-                    })
-                    .catch(err => console.log(chalk.bgRed(err)));
-            });
-
-        let viewTablesPromise = query.getViewTables(connection, query.escapeQuotes)
-            .then(viewTables => {
-                file.getViewTablesTemplate(viewTables, config, ejs)
-                    .then(html => {
-                        let fileName = `${(new Date).getTime()}${length + 1}_create_view_tables.php`;
-                        file.generateFile(html, fileName, config, fs)
-                            .then(fileName => {
-                                util.log(`View tables was generated successfully`);
-                            })
-                            .catch(err => console.log(chalk.bgRed(err)));
-                    });
-            })
-            .catch(err => console.log(chalk.bgRed(err)));
-
-        let proceduresPromise = query.getProcedures(connection, query.convertProceduresToObjects, query.escapeQuotes)
-            .then(procedures => {
-                file.getProcedureTemplate(procedures, config, ejs)
-                    .then(html => {
-                        let fileName = `${(new Date).getTime()}${length + 2}_add_procedures_and_functions.php`;
-                        file.generateFile(html, fileName, config, fs)
-                            .then(fileName => {
-                                util.log(`Procedures and functions was generated successfully`);
-                            })
-                            .catch(err => console.log(chalk.bgRed(err)));
-                    });
-            })
-            .catch(err => console.log(chalk.bgRed(err)));
-
-        let triggersPromise = query.getTriggers(connection, query.escapeQuotes, _)
-            .then(triggers => {
-                file.getTriggersTemplate(triggers, config, ejs)
-                    .then(html => {
-                        let fileName = `${(new Date).getTime()}${length + 3}_add_triggers.php`;
-                        file.generateFile(html, fileName, config, fs)
-                            .then(fileName => {
-                                util.log(`Triggers was generated successfully`);
-                            })
-                            .catch(err => console.log(err));
-                    });
-            })
-            .catch(err => console.log(chalk.bgRed(err)));
-
-        Promise.all([foreignKeyPromise, proceduresPromise, viewTablesPromise, triggersPromise])
-            .then(res => {
-                connection.end();
-            })
-            .catch(err => console.log(chalk.bgRed(err)));
+Promise.all([tableDataPromise, proceduresPromise, viewTablesPromise, triggersPromise, foreignKeyTemplate])
+    .then(res => {
+        connection.end()
+        util.log(chalk.green(`All Done.`));
     })
     .catch(err => console.log(chalk.bgRed(err)));
