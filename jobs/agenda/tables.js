888c075962e28e3d26a0e37530a9fa2f6086f99e
Joo Martin <m4rt1n.j00@gmail.com>
2017-06-04 22:38:50 +0200

    agenda

---
+++ jobs/agenda/tables.js
@@ -0,0 +1,19 @@
+const Agenda = require('agenda');
+
+const createTablesJob = require('../database/tables/create');
+
+const agenda = new Agenda({ db: { address: 'mongodb://127.0.0.1:27017/agenda' } });
+
+agenda.define('create new tables', (job, done) => {
+    createTablesJob.run()
+        .then(res => {
+            console.log(`${res.result.n} migration(s) was generated for new table`);        
+            done()
+        });
+});
+
+agenda.on('ready', function() {
+    agenda.every('5 seconds', 'create new tables', { timezone: 'Europe/Budapest' });
+ 
+    agenda.start();
+});
---
+++ jobs/database/tables/create.js
@@ -0,0 +1,68 @@
+const MongoClient = require('mongodb').MongoClient;
+const _ = require('lodash');
+
+const query = require('../../../database/query');
+const queryProcess = require('../../../business/query-process');
+const connection = require('../../../database/connection');
+const config = require('../../../config.json');
+const utils = require('../../../utils/utils');
+
+const url = 'mongodb://localhost:27017/migration-generator';
+
+const run = () => {
+    return new Promise((resolve, reject) => {
+        MongoClient.connect(url, (err, db) => {
+            if (err) throw err;
+
+            let allTables = [];
+            let allCachedTables = [];
+
+            query.getTables(connection, config, queryProcess.mapTables)
+                .then(utils.sideEffect(tables => allTables = tables))
+                .then(tables => getCachedTables(db, config))
+                .then(cachedTables => {
+                    return cachedTables.length === 0
+                        ? allTables : _.difference(allTables, cachedTables.map(t => t.name));
+                })
+                .then(tablesToInsert => insertTables(db, config, tablesToInsert))
+                .then(res => resolve(res))
+                .catch(err => reject(err));
+        });
+    });
+}
+
+/**
+ * @param {Object} db 
+ * @param {Object} config 
+ */
+const getCachedTables = (db, config) => {
+    return new Promise((resolve, reject) => {
+        db.collection('tables').find({ database: config.database }).toArray((err, docs) => {
+            if (err) return reject(err);
+
+            resolve(docs);
+        });
+    });
+}
+
+/**
+ * @param {Object} db 
+ * @param {Object} config 
+ * @param {Array} tables 
+ */
+const insertTables = (db, config, tables) => {
+    return new Promise((resolve, reject) => {
+        if (tables.length === 0) return resolve({ result: { ok: 1, n: 0 }});
+        const mappedTables = tables.map(t => ({ name: t, database: config.database }));
+
+        db.collection('tables').insertMany(mappedTables, (err, result) => {
+            if (err) return reject(err);
+
+            resolve(result);
+        });
+    });
+}
+
+module.exports = {
+    run
+}
