431891defefcc8b9237858488887c7a06cd5a77e
Joo Martin <m4rt1n.j00@gmail.com>
2017-06-04 22:15:46 +0200

    Inserting newly created tables

--- jobs/tables/create.js
+++ jobs/tables/create.js
@@ -5,29 +5,32 @@ const query = require('../../database/query');
 const queryProcess = require('../../business/query-process');
 const connection = require('../../database/connection');
 const config = require('../../config.json');
+const utils = require('../../utils/utils');
 
 const url = 'mongodb://localhost:27017/migration-generator';
 
 MongoClient.connect(url, (err, db) => {
     if (err) throw err;
 
+    let allTables = [];
+    let allCachedTables = [];
+
     query.getTables(connection, config, queryProcess.mapTables)
-        .then(tables => {
-            getCachedTables(db, config)
-                .then(cachedTables => {
-                    if (cachedTables.length === 0) {
-                        initTables(db, config, tables)
-                            .then(console.log)
-                            .catch(console.log);
-                    } else {
-                        const cachedTableNames = cachedTables.map(t => t.name);
-                        console.log(_.difference(tables, cachedTableNames));
-                    }
-                });
+        .then(utils.sideEffect(tables => allTables = tables))
+        .then(tables => getCachedTables(db, config))
+        .then(cachedTables => {
+            return cachedTables.length === 0
+                ? tables : _.difference(allTables, cachedTables.map(t => t.name));
         })
+        .then(tablesToInsert => insertTables(db, config, tablesToInsert))
+        .then(console.log)
         .catch(console.log);
 });
 
+/**
+ * @param {Object} db 
+ * @param {Object} config 
+ */
 const getCachedTables = (db, config) => {
     return new Promise((resolve, reject) => {
         db.collection('tables').find({ database: config.database }).toArray((err, docs) => {
@@ -38,8 +41,14 @@ const getCachedTables = (db, config) => {
     });
 }
 
-const initTables = (db, config, tables) => {
+/**
+ * @param {Object} db 
+ * @param {Object} config 
+ * @param {Array} tables 
+ */
+const insertTables = (db, config, tables) => {
     return new Promise((resolve, reject) => {
+        if (tables.length === 0) return resolve(true);
         const mappedTables = tables.map(t => ({ name: t, database: config.database }));
 
         db.collection('tables').insertMany(mappedTables, (err, result) => {
