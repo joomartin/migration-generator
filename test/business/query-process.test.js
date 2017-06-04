4b796d266d060195deb20f2f7eb22e4d77598b8f
Joo Martin <m4rt1n.j00@gmail.com>
2017-06-04 18:05:30 +0200

    QueryProcess test

--- test/business/query-process.test.js
+++ test/business/query-process.test.js
@@ -1,4 +1,7 @@
 const expect = require('chai').expect;
+const _ = require('lodash');
+
+const strUtils = require('../../utils/str');
 const queryProcess = require('../../business/query-process');
 
 describe('QueryProcess', () => {
@@ -206,4 +209,32 @@ describe('QueryProcess', () => {
             ]);
         });
     });
+
+    describe('#getDependenciesFromCreateTable()', () => {
+        it('should return an object that contains all foreign keys and meta data for a table', () => {
+            const dependencies = queryProcess.getDependenciesFromCreateTable(
+                _, strUtils.substringFrom, 'todos', createTable); 
+
+            expect(dependencies).to.be.lengthOf(2);
+            expect(dependencies).to.be.deep.equal([
+                {
+                    sourceTable: 'todos',
+                    sourceColumn: 'category_id',
+                    referencedTable: 'categories',
+                    referencedColumn: 'id',
+                    updateRule: 'NO ACTION',
+                    deleteRule: 'SET NULL'
+                }, {
+                    sourceTable: 'todos',
+                    sourceColumn: 'user_id',
+                    referencedTable: 'users',
+                    referencedColumn: 'id',
+                    updateRule: 'SET NULL',
+                    deleteRule: 'CASCADE'
+                },
+            ]);          
+        });
+    });
 });
+
+const createTable = "CREATE TABLE `todos` ( `id` int(11) unsigned NOT NULL AUTO_INCREMENT, `title` varchar(100) DEFAULT NULL, `category_id` int(11) unsigned DEFAULT NULL, `hours` decimal(10,2) unsigned DEFAULT NULL, `description` longtext, `is_done` tinyint(1) DEFAULT NULL, `unique_id` tinyint(1) DEFAULT NULL, `user_id` int(11) unsigned DEFAULT NULL, PRIMARY KEY (`id`), UNIQUE KEY `unique_id` (`unique_id`), KEY `category_id` (`category_id`), KEY `is_done` (`is_done`), KEY `user_id` (`user_id`), CONSTRAINT `todos_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION, CONSTRAINT `todos_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE SET NULL ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8";
