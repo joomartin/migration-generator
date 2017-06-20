fc2da4e9ed2bb7556da001e79a69f47f0b1828cc
Joo Martin <m4rt1n.j00@gmail.com>
2017-06-20 13:52:16 +0200

    composeP

--- app.js
+++ app.js
@@ -2,82 +2,62 @@ const fs = require('fs');
 const chalk = require('chalk');
 const ejs = require('ejs');
 const util = require('util');
-const { map, tap, compose, composeP, __ } = require('ramda');
+const { map, tap, compose, composeP, __, length } = require('ramda');
 
+const config = require('./config.json');
 const connection = require('./database/connection');
 const columnInfoFactory = require('./database/column-info/factory');
-const query = require('./database/query');
+
 const file = require('./file/file');
-const utils = require('./utils/utils');
-const strUtils = require('./utils/str');
+const { getDate , getSerial, logHeader } = require('./utils/utils');
+const { getViewTables, getProcedures, getTriggers, getTableData } = require('./database/query');
 const { normalizeProcedureDefinition, sanitizeViewTables, mapTriggers } = require('./business/query-process');
 const { generateFile, getViewTablesTemplate, getProcedureTemplate, getTriggersTemplate, getFileNames, getTemplates, generateFiles, getForeignKeyTemplate } = file;
-const config = require('./config.json');
 
-utils.logHeader(config, util, console, chalk);
+
+logHeader(config, util, console, chalk);
 
 let allTables = [];
 let fileNames = [];
 
 const viewTablesPromise = composeP(
     tap(filename => console.log(`${filename} was generated successfully`)),
-    generateFile(fs, config, `${utils.getDate()}${utils.getSerial(990)}_create_view_tables.php`),
+    generateFile(fs, config, `${getDate()}${getSerial(990)}_create_view_tables.php`),
     getViewTablesTemplate(ejs, config),
     sanitizeViewTables(config.database),
-    query.getViewTables,
+    getViewTables,
 )(connection);
 
 const proceduresPromise = composeP(
     tap(filename => console.log(`${filename} was generated successfully`)),
-    generateFile(fs, config, `${utils.getDate()}${utils.getSerial(991)}_create_procedures.php`),
+    generateFile(fs, config, `${getDate()}${getSerial(991)}_create_procedures.php`),
     getProcedureTemplate(ejs, config),
     map(normalizeProcedureDefinition),
-    query.getProcedures
+    getProcedures
 )(connection);
 
 const triggersPromise = composeP(
     tap(filename => console.log(`${filename}_create_view_tables.php was generated successfully`)),
-    generateFile(fs, config, `${utils.getDate()}${utils.getSerial(992)}_create_triggers.php`),
+    generateFile(fs, config, `${getDate()}${getSerial(992)}_create_triggers.php`),
     getTriggersTemplate(ejs, config),
     mapTriggers(config.database),
-    query.getTriggers
+    getTriggers
 )(connection);
 
-// const tablesPromise = query.getTableData(connection, config)
-//     .then(tap(tables => allTables = tables));
-
-// const tablesPromise = composeP(
-//     tap(tables => allTables = tables),
-//     query.getTableData(connection)
-// )(config);
-
-// tablesPromise.then(console.log);
-
 const generateTablesPromise = composeP(
-    (files) => files.length,
+    tap(filename => console.log(`${filename} was generated successfully`)),    
+    generateFile(fs, config, `${getDate()}${getSerial(993)}_add_foreign_keys.php`),    
+    (_) => getForeignKeyTemplate(ejs, config, allTables),    
     (templates) => generateFiles(fs, file, config, fileNames, templates),
     (_) => getTemplates(ejs, file, config, columnInfoFactory, allTables),
     tap(fns => fileNames = fns),
     getFileNames(new Date, file),
     tap(tables => allTables = tables),    
-    query.getTableData(connection)
+    getTableData(connection)
 )(config);
 
-// const generateTablesPromise = tablesPromise
-//     .then(getFileNames(new Date, file))
-//     .then(tap(fns => fileNames = fns))
-//     .then(_ => getTemplates(ejs, file, config, columnInfoFactory, allTables))
-//     .then(templates => generateFiles(fs, file, config, fileNames, templates))
-//     .then(files => files.length)
-//     .catch(err => console.log(chalk.bgRed(err)));
-
-const foreignKeyPromise = generateTablesPromise
-    .then(_ => getForeignKeyTemplate(ejs, config, allTables))
-    .then(generateFile(fs, config, `${utils.getDate()}${utils.getSerial(993)}_add_foreign_keys.php`))
-    .then(tap(filename => console.log(`${filename} was generated successfully`)));
-
-Promise.all([proceduresPromise, viewTablesPromise, triggersPromise, foreignKeyPromise, generateTablesPromise])
-    .then(([proc, vt, tr, fks, tablesCount]) => console.log(chalk.green(`${tablesCount} tables was generated successfully`)))
+Promise.all([proceduresPromise, viewTablesPromise, triggersPromise, generateTablesPromise])
+    .then(_ => console.log(chalk.green(`${allTables.length} tables was generated successfully`)))
     .then(_ => connection.end())
     .then(tap(_ => util.log(chalk.green(`All Done.`))))
     .catch(err => console.log(chalk.bgRed(err)));
