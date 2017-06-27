a49cfbaa0d395def43225c6ed4fdcd2c5f5f3cc6
Joo Martin <m4rt1n.j00@gmail.com>
2017-06-27 21:08:26 +0200

    composeP and clean up

--- jobs/database/tables/create.js
+++ jobs/database/tables/create.js
@@ -1,5 +1,4 @@
-// const MongoClient = require('mongodb').MongoClient;
-const { curry, difference, tap, map, prop, __, composeP } = require('ramda');
+const { curry, difference, tap, map, prop, __, composeP, compose } = require('ramda');
 
 const { mongoose } = require('../../../config/mongoose');
 const { Table } = require('../../../models/table')
@@ -10,47 +9,31 @@ const { mapTables } = require('../../../business/query-process');
 
 const url = 'mongodb://localhost:27017/migration-generator';
 
-// queryGenerator :: Object -> String -> String -> Object
-const queryGenerator = curry((db, queryType, collection, queryObj) =>
-    new Promise((resolve, reject) =>
-        db.collection(collection)[queryType](queryObj).toArray((err, docs) =>
-            err ? reject(err) : resolve(docs))));
-
 // run :: void -> Promise
-const run = () =>
-    new Promise((resolve, reject) => {
-        let allTables = [];
+const run = () => {
+    let allTables = [];
 
-        const insertTablesPromise = composeP(
-            insertTables(config),
-            cachedTables => difference(allTables, cachedTables),
-            map(prop('name')),
-            _ => getCachedTables(config.database),
-            tap(tables => allTables = tables),
-            mapTables(config),
-            getTables
-        )(connection);
+    return composeP(
+        insertTables(config),
+        cachedTables => difference(allTables, cachedTables),
+        map(prop('name')),
+        _ => getCachedTables(config.database),
+        tap(tables => allTables = tables),
+        mapTables(config),
+        getTables
+    )(connection);
+};
 
-        insertTablesPromise
-            .then(resolve)
-            .catch(reject);
-    });
-
-// getCachedTables :: Object -> String -> Promise
-// const getCachedTables = (db, database) => queryGenerator(db, 'find', 'tables', { database });
+// getCachedTables :: String -> Promise
 const getCachedTables = (database) => Table.find({ database });
 
 // insertTables :: Object -> Array -> Promise
-const insertTables = curry((config, tables) => {
-        if (tables.length === 0)
-            return resolve({ result: { ok: 1, n: 0 } });
-
-        const mappedTables = tables.map(t => ({ name: t, database: config.database }));
-        return Table.insertMany(mappedTables);
-
-        // db.collection('tables').insertMany(mappedTables, (err, result) =>
-        //     err ? reject(err) : resolve(result));
-    });
+const insertTables = curry((config, tables) =>
+    compose(
+        Table.insertMany,
+        map(t => ({ name: t, database: config.database }))
+    )(tables)
+);
 
 module.exports = {
     run, getCachedTables, insertTables, url
